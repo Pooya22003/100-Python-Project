4c1e9b88b7b5523ebb7d7e758f95a21735327d8c
Pooya <pooyakord47@gmail.com>
2026-05-29 10:57:05 +0330

    Add files via upload

---
+++ Band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+City_Name = input("What is the name of the city you Grew up ?\n")
+Pet_Name = input ("what is the name of a pet ?\n")
+print("Your Band name could be =" , City_Name + " " + Pet_Name)
---
+++ tip_calculator.py
@@ -0,0 +1,15 @@
+print("Welcome to the tip calculator.")
+bill = float(input("What is the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10, 12 or 15? "))
+people = int(input("How many people to spli the bill?"))
+tip_as_percent = tip / 100
+total_tip_amount = bill * tip_as_percent
+total_bill = bill + total_tip_amount
+bill_per_person = total_bill / people
+#final_amount = "{:.2f}".format(bill_per_person)
+final_amountt = round(bill_per_person, 2)
+print(f"each person should pay ${final_amountt}")
+
+
+
+
